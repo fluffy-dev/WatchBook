8b718b2576501f6f3d15f7e7488ad471b70bcee4
Rauan55 <asetovrauan95@gmail.com>
2025-04-13 16:29:23 +0500

    base sqlalchemy model

---
+++ backend/src/libs/base_model.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from sqlalchemy import TIMESTAMP, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    """
+    Basic SqlAlchemy Model
+
+    fields:
+    - id: autoincrement int
+    - created_at: datetime - creates and updates once
+    - updated_at: datetime - creates once and updates every time
+
+    """
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True, unique=True, autoincrement=True)
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
